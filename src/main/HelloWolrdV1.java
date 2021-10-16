05bd2f7701320ac26e13cc1e2954512b7c911e49
tarunnagula <shivatarunkumar@gmail.com>
2021-10-16 17:06:33 +0530

    Feat: first commit to git

---
+++ src/main/HelloWolrdV1.java
@@ -0,0 +1,9 @@
+package main;
+
+import java.sql.SQLOutput;
+
+public class HelloWolrdV1 {
+    public static void main(String[] args) {
+        System.out.println("Hellow Worlds version V1");
+    }
+}
