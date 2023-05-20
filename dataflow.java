82c05c44afdaa5041d0b0d3dcad4bb3465eebc14
shivatarunkumar <shivatarunkumar@gmail.com>
2023-05-21 01:24:26 +0530

    Update dataflow.java

--- dataflow.java
+++ dataflow.java
@@ -1,30 +1,57 @@
 import com.google.api.services.dataflow.Dataflow;
 import com.google.api.services.dataflow.model.Job;
 import com.google.api.services.dataflow.model.JobMetrics;
+import com.google.api.services.dataflow.model.ListJobsResponse;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.dataflow.sdk.util.MonitoringUtil;
+
+import java.io.IOException;
+import java.util.List;
 
 public class DataflowJobStatus {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         String projectId = "your-project-id";
-        String jobId = "bq-to-spanner";
+        String jobName = "bq-to-spanner";
 
-        Dataflow dataflowService = DataflowServiceOptions.newBuilder()
-                .setProjectId(projectId)
-                .build()
-                .getService();
+        // Authenticate using Application Default Credentials (ADC)
+        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
 
-        // Get the job details
-        Job job = dataflowService.projects().jobs().get(projectId, jobId).execute();
+        // Create a Dataflow client
+        Dataflow dataflowService = new Dataflow.Builder(credentials.getTransport(), credentials.getJsonFactory(), credentials)
+                .setApplicationName("Dataflow Job Status Checker")
+                .build();
 
-        // Check if the job is running
-        String jobState = job.getCurrentState();
-        if (jobState.equalsIgnoreCase("JOB_STATE_RUNNING")) {
-            System.out.println("The Dataflow job is currently running.");
-        } else {
-            System.out.println("The Dataflow job is not running. Current state: " + jobState);
+        // List jobs in the project
+        ListJobsResponse response = dataflowService.projects().jobs().list(projectId).execute();
+        List<Job> jobs = response.getJobs();
+
+        // Find the job with the specified name
+        Job targetJob = null;
+        for (Job job : jobs) {
+            if (job.getName().equals(jobName)) {
+                targetJob = job;
+                break;
+            }
         }
 
-        // You can also retrieve and print additional job information if needed
-        JobMetrics jobMetrics = dataflowService.projects().jobs().getMetrics(projectId, jobId).execute();
-        // Print job metrics or other relevant information
+        // Check if the job was found
+        if (targetJob != null) {
+            // Get the job details
+            Job jobDetails = dataflowService.projects().jobs().get(projectId, targetJob.getId()).execute();
+
+            // Check the current state of the job
+            String jobState = jobDetails.getCurrentState();
+            if (jobState.equalsIgnoreCase("JOB_STATE_RUNNING")) {
+                System.out.println("The Dataflow job is currently running.");
+            } else {
+                System.out.println("The Dataflow job is not running. Current state: " + jobState);
+            }
+
+            // Retrieve and print additional job information if needed
+            JobMetrics jobMetrics = dataflowService.projects().jobs().getMetrics(projectId, targetJob.getId()).execute();
+            MonitoringUtil.printMetrics(jobMetrics);
+        } else {
+            System.out.println("The Dataflow job with the specified name was not found.");
+        }
     }
 }
